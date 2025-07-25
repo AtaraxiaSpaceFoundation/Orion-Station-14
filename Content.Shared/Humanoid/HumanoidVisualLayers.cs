AtaraxiaSpaceFoundation/space-station-14

[Port] Better Lizards

Content.Shared/Humanoid/HumanoidVisualLayers.cs
@@ -15,6 +15,7 @@ namespace Content.Shared.Humanoid
         Chest,
         Head,
         Snout,
+        SnoutOverlay, // Backmen | for animated tongue
         HeadSide, // side parts (i.e., frills)
         HeadTop,  // top parts (i.e., ears)
         Eyes,

Content.Shared/Humanoid/Markings/MarkingCategories.cs
@@ -12,6 +12,7 @@ namespace Content.Shared.Humanoid.Markings
         HeadTop,
         HeadSide,
         Snout,
+        SnoutOverlay, // Backmen
         Chest,
         UndergarmentTop,
         UndergarmentBottom,
@@ -34,6 +35,7 @@ namespace Content.Shared.Humanoid.Markings
                 HumanoidVisualLayers.HeadTop => MarkingCategories.HeadTop,
                 HumanoidVisualLayers.HeadSide => MarkingCategories.HeadSide,
                 HumanoidVisualLayers.Snout => MarkingCategories.Snout,
+                HumanoidVisualLayers.SnoutOverlay => MarkingCategories.SnoutOverlay, // Backmen
                 HumanoidVisualLayers.Chest => MarkingCategories.Chest,
                 HumanoidVisualLayers.UndergarmentTop => MarkingCategories.UndergarmentTop,
                 HumanoidVisualLayers.UndergarmentBottom => MarkingCategories.UndergarmentBottom,

AtaraxiaSpaceFoundation/Orion-Station-14

Content.Shared/Humanoid/HumanoidVisualLayers.cs
@@ -15,6 +15,7 @@ namespace Content.Shared.Humanoid
         Chest,
         Head,
         Snout,
+        SnoutOverlay, // Europa | for animated tongue
         HeadSide, // side parts (i.e., frills)
         HeadTop,  // top parts (i.e., ears)
         Eyes,

Content.Shared/Humanoid/Markings/MarkingCategories.cs
@@ -12,6 +12,7 @@ namespace Content.Shared.Humanoid.Markings
         HeadTop,
         HeadSide,
         Snout,
+        SnoutOverlay, // Europa
         Chest,
         UndergarmentTop,
         UndergarmentBottom,
@@ -34,6 +35,7 @@ namespace Content.Shared.Humanoid.Markings
                 HumanoidVisualLayers.HeadTop => MarkingCategories.HeadTop,
                 HumanoidVisualLayers.HeadSide => MarkingCategories.HeadSide,
                 HumanoidVisualLayers.Snout => MarkingCategories.Snout,
+                HumanoidVisualLayers.SnoutOverlay => MarkingCategories.SnoutOverlay, // Europa
                 HumanoidVisualLayers.Chest => MarkingCategories.Chest,
                 HumanoidVisualLayers.UndergarmentTop => MarkingCategories.UndergarmentTop,
                 HumanoidVisualLayers.UndergarmentBottom => MarkingCategories.UndergarmentBottom,