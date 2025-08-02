59bd1de16d0d0f50bf5e1a6c1acfa3404926a07f
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-08-02 16:42:54 +0300

    Rly working summon action

--- Content.Shared/_Europa/Abilities/Goliath/GoliathSummonBabyAction.cs
+++ Content.Shared/_Europa/Abilities/Goliath/GoliathSummonBabyAction.cs
@@ -3,11 +3,17 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Abilities.Goliath;
 
-public sealed partial class GoliathSummonBabyAction : EntityTargetActionEvent
+public sealed partial class GoliathSummonBabyAction : EntityWorldTargetActionEvent
 {
     [DataField]
     public EntProtoId EntityId = "MobGoliathBaby";
 
     [DataField]
     public int SummonCount = 2;
+
+    [DataField]
+    public List<Direction> OffsetDirections = new();
+
+    [DataField]
+    public float Offset = 4f;
 }
--- Content.Shared/_Europa/Abilities/Goliath/Systems/GoliathSummonSystem.cs
+++ Content.Shared/_Europa/Abilities/Goliath/Systems/GoliathSummonSystem.cs
@@ -1,4 +1,8 @@
 using System.Numerics;
+using Content.Shared.Maps;
+using Content.Shared.Physics;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
 using Robust.Shared.Network;
 using Robust.Shared.Random;
 
@@ -8,6 +12,9 @@ public sealed class GoliathSpawnSystem : EntitySystem
 {
     [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly SharedMapSystem _map = default!;
+    [Dependency] private readonly TurfSystem _turf = default!;
 
     /// <inheritdoc/>
     public override void Initialize()
@@ -17,25 +24,31 @@ public sealed class GoliathSpawnSystem : EntitySystem
 
     private void OnSummonAction(GoliathSummonBabyAction args)
     {
-        if (args.Handled)
+        if (args.Handled || args.Coords is not { } coords)
             return;
 
-        var summonCount = Math.Clamp(args.SummonCount, 1, 8);
-        var performer = args.Performer;
-        var xform = Transform(performer);
-        var coords = xform.Coordinates;
-        var mapUid = xform.MapUid;
+        List<EntityCoordinates> spawnPositions = new();
+        spawnPositions.Add(coords);
 
-        if (mapUid == null)
-            return;
-
-        for (var i = 0; i < summonCount; i++)
+        for (var i = 0; i < 1; i++)
         {
-            var offset = new Vector2(_random.NextFloat(-1f, 1f), _random.NextFloat(-1f, 1f));
-            var spawnCoords = coords.Offset(offset);
+            var spawnPos = coords.Offset(_random.NextVector2(args.Offset));
+            spawnPositions.Add(spawnPos);
+        }
+
+        foreach (var pos in spawnPositions)
+        {
+            if (_transform.GetGrid(pos) is not { } grid ||
+                !TryComp<MapGridComponent>(grid, out var gridComp) ||
+                !_map.TryGetTileRef(grid, gridComp, pos, out var tileRef) ||
+                tileRef.IsSpace() ||
+                _turf.IsTileBlocked(tileRef, CollisionGroup.Impassable))
+            {
+                continue;
+            }
 
             if (_net.IsServer)
-                Spawn(args.EntityId, spawnCoords);
+                Spawn(args.EntityId, pos);
         }
 
         args.Handled = true;
