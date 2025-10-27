ec6d803211ac7bfe9758092c7d009636b26e650c
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-10-27 18:56:17 +0300

    [Feature] Data In PDA

---
+++ Content.Shared/_Orion/Time/TimeSystem.cs
@@ -0,0 +1,38 @@
+using Content.Shared.GameTicking;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._Orion.Time;
+
+public sealed class TimeSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+    private TimeSpan _roundStart = TimeSpan.Zero;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeNetworkEvent<TickerLobbyStatusEvent>(OnLobbyStatus);
+    }
+
+    private void OnLobbyStatus(TickerLobbyStatusEvent ev)
+    {
+        _roundStart = ev.RoundStartTimeSpan;
+    }
+
+    public (TimeSpan Time, DateTime Date) GetStationTime()
+    {
+        var elapsed = _timing.CurTime.Subtract(_roundStart);
+        var totalHours = elapsed.TotalHours;
+
+        var days = (int)(totalHours / 24);
+        var remainingHours = totalHours % 24;
+
+        var startDate = DateTime.UtcNow.Date;
+        var futureYear = startDate.Year + 500;
+        var stationDate = new DateTime(futureYear, startDate.Month, startDate.Day).AddDays(days);
+
+        return (TimeSpan.FromHours(remainingHours), stationDate);
+    }
+}
