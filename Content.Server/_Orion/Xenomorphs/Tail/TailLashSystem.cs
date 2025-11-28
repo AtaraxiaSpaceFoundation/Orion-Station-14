87b6d1e8f7cdd75a24eda7385881373f4369a935
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-11-29 01:47:32 +0300

    better tail lash

---
+++ Content.Server/_Orion/Xenomorphs/Tail/TailLashSystem.cs
@@ -0,0 +1,48 @@
+﻿using Content.Shared._Orion.Xenomorphs.Tail;
+using Content.Shared.Actions;
+using Content.Shared.Standing;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Server._Orion.Xenomorphs.Tail;
+
+//
+// License-Identifier: AGPL-3.0-or-later
+//
+
+public sealed class TailLashSystem : EntitySystem
+{
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly StandingStateSystem _standing = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<TailLashComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<TailLashComponent, ComponentShutdown>(OnComponentShutdown);
+        SubscribeLocalEvent<TailLashComponent, TailLashActionEvent>(OnLash);
+    }
+
+    private void OnComponentInit(EntityUid uid, TailLashComponent component, ComponentInit args)
+    {
+        _actions.AddAction(uid, ref component.TailLashAction, component.TailLashActionId, uid);
+    }
+
+    private void OnComponentShutdown(EntityUid uid, TailLashComponent component, ComponentShutdown args)
+    {
+        _actions.RemoveAction(uid, component.TailLashAction);
+    }
+
+    private void OnLash(EntityUid uid, TailLashComponent component, TailLashActionEvent args)
+    {
+        foreach (var entity in _lookup.GetEntitiesInRange(uid, component.LashRange))
+        {
+            if (HasComp<StandingStateComponent>(entity))
+                _standing.Down(entity);
+        }
+
+        _audio.PlayPredicted(component.LashSound, uid, uid);
+    }
+}
---
+++ Content.Shared/_Orion/Xenomorphs/Tail/TailLashComponent.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared.Actions;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Orion.Xenomorphs.Tail;
+
+//
+// License-Identifier: AGPL-3.0-or-later
+//
+
+[RegisterComponent]
+public sealed partial class TailLashComponent : Component
+{
+    [DataField]
+    public EntProtoId? TailLashActionId = "ActionTailLash";
+
+    [ViewVariables]
+    public EntityUid? TailLashAction;
+
+    [DataField]
+    public float LashRange = 2f;
+
+    [DataField]
+    public int StunTime = 1;
+
+    [DataField]
+    public SoundSpecifier LashSound = new SoundPathSpecifier("/Audio/Effects/thudswoosh.ogg", AudioParams.Default.WithVolume(-3));
+}
+
+public sealed partial class TailLashActionEvent : InstantActionEvent;
