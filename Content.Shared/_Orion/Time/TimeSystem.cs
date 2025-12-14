37f86ffae00c89c192e1d18c3e126399b3f297bb
PuroSlavKing <puroslavking@yahoo.com>
2025-12-15 02:33:23 +0300

    randomized time + time accumulation

--- Content.Shared/_Orion/Time/TimeSystem.cs
+++ Content.Shared/_Orion/Time/TimeSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
+using Robust.Shared.Random;
 
 namespace Content.Shared._Orion.Time;
 
@@ -10,10 +11,14 @@ namespace Content.Shared._Orion.Time;
 public sealed class TimeSystem : EntitySystem
 {
     [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IRobustRandom _robustRandom = default!;
 
     private int _yearOffset;
     private int _staticYear;
     private bool _useStaticYear;
+
+    private TimeSpan _stationTime; // Handle time
+    private float _accumulatedSeconds;
 
     public override void Initialize()
     {
@@ -22,6 +27,26 @@ public sealed class TimeSystem : EntitySystem
         _cfg.OnValueChanged(CCVars.StationTimeOffsetYears, v => _yearOffset = v, true);
         _cfg.OnValueChanged(CCVars.StationTimeUseStaticYear, v => _useStaticYear = v, true);
         _cfg.OnValueChanged(CCVars.StationTimeStaticYear, v => _staticYear = v, true);
+
+        _stationTime = TimeSpan.FromHours(_robustRandom.NextFloat(0, 24));
+    }
+
+    // Accumulate time faster
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        _accumulatedSeconds += frameTime * 2f; // x2 speed
+
+        var delta = TimeSpan.FromSeconds(_accumulatedSeconds);
+        _stationTime += delta;
+
+        if (_stationTime >= TimeSpan.FromDays(1))
+            _stationTime = _stationTime.Subtract(TimeSpan.FromDays(1));
+        else if (_stationTime < TimeSpan.Zero)
+            _stationTime = _stationTime.Add(TimeSpan.FromDays(1));
+
+        _accumulatedSeconds -= (float)delta.TotalSeconds;
     }
 
     public DateTime GetStationDate()
@@ -44,8 +69,8 @@ public sealed class TimeSystem : EntitySystem
         return stationDate;
     }
 
-    public TimeSpan GetStationTime() // TODO: Randomize this?
+    public TimeSpan GetStationTime()
     {
-        return DateTime.UtcNow.TimeOfDay;
+        return _stationTime;
     }
 }
