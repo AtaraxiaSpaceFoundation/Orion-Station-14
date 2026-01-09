WWhiteDreamProject/wwdpublic

[Feature] Better Ghost UI (#1011)
* - add: ghost teleport menu https://github.com/AtaraxiaSpaceFoundation/Orion-Station-14/pull/107

* - tweak: Stylebox thinks and styles remove

* - fix: Зайкины фиксы ч.1

* - fix: Кролькины фиксы ч.2

* - fix: кролькины фиксы ч.3

* - fix: Исправление логических ошибок и перенос в WWDP

* - fix: Кролькины фиксы ч.4

* Apply suggestions from code review

* - tweak: перелопатил всю логику

* - fix: Кролькины фиксы ч.5

* - fix: Кролькины фиксы ч.6

* - fix: спатисоны фиксы отображения департментов

* - fix: department description

* - add: some validation

* - fix: теперь и другие мобы показываются тут

---------

Co-authored-by: PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

Content.Client/UserInterface/Systems/Ghost/Controls/GhostTargetWindow.xaml.cs
@@ -1,7 +1,8 @@
-using System.Linq;
-using System.Numerics;
+using Content.Client._White.UserInterface.Controls;
+using Content.Client.UserInterface.Controls;
 using Content.Shared.Ghost;
 using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Client.UserInterface.XAML;
@@ -11,8 +12,21 @@ namespace Content.Client.UserInterface.Systems.Ghost.Controls
     [GenerateTypedNameReferences]
     public sealed partial class GhostTargetWindow : DefaultWindow
     {
-        private List<(string, NetEntity)> _warps = new();
         private string _searchText = string.Empty;
+
+        // WWDP EDIT START
+        private List<GhostWarp> _originalGhostWarps = [];
+
+        private Dictionary<string, List<GhostWarp>> _alivePlayers = [];
+        private Dictionary<string, List<GhostWarp>> _deadPlayers = [];
+        private Dictionary<string, List<GhostWarp>> _ghostPlayers = [];
+        private Dictionary<string, List<GhostWarp>> _leftPlayers = [];
+
+        private Dictionary<string, List<GhostWarp>> _aliveAntags = [];
+        private Dictionary<string, List<GhostWarp>> _places = [];
+        private Dictionary<string, List<GhostWarp>> _other = [];
+
+        // WWDP EDIT END
 
         public event Action<NetEntity>? WarpClicked;
         public event Action? OnGhostnadoClicked;
@@ -25,71 +39,214 @@ namespace Content.Client.UserInterface.Systems.Ghost.Controls
             GhostnadoButton.OnPressed += _ => OnGhostnadoClicked?.Invoke();
         }
 
-        public void UpdateWarps(IEnumerable<GhostWarp> warps)
-        {
-            // Server COULD send these sorted but how about we just use the client to do it instead
-            _warps = warps
-                .OrderBy(w => w.IsWarpPoint)
-                .ThenBy(w => w.DisplayName, Comparer<string>.Create(
-                    (x, y) => string.Compare(x, y, StringComparison.Ordinal)))
-                .Select(w =>
-                {
-                    var name = w.IsWarpPoint
-                        ? Loc.GetString("ghost-target-window-current-button", ("name", w.DisplayName))
-                        : w.DisplayName;
-
-                    return (name, w.Entity);
-                })
-                .ToList();
-        }
-
         public void Populate()
         {
-            ButtonContainer.DisposeAllChildren();
+            // WWDP EDIT START
+            GhostTeleportContainer.DisposeAllChildren();
+            FilterWarps();
             AddButtons();
+            // WWDP EDIT END
+        }
+
+        // WWDP EDIT START
+        private void FilterWarps()
+        {
+
+            _alivePlayers.Clear();
+            _deadPlayers.Clear();
+            _ghostPlayers.Clear();
+            _leftPlayers.Clear();
+            _aliveAntags.Clear();
+            _places.Clear();
+            _other.Clear();
+
+            foreach (var warp in _originalGhostWarps)
+            {
+                if(!string.IsNullOrEmpty(_searchText) && !warp.DisplayName.Contains(_searchText, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                if(warp.Group == WarpGroup.Location)
+                {
+                    FilterLocalWarps(_places, warp, null);
+                    continue;
+                }
+
+                FilterLocalWarps(_alivePlayers, warp, WarpGroup.AliveDepartment);
+                FilterLocalWarps(_deadPlayers, warp, WarpGroup.DeadDepartment);
+                FilterLocalWarps(_leftPlayers, warp, WarpGroup.LeftDepartment);
+
+                FilterLocalWarps(_aliveAntags, warp, WarpGroup.AliveAntag);
+                FilterLocalWarps(_deadPlayers, warp, WarpGroup.DeadAntag);
+
+                FilterLocalWarps(_ghostPlayers, warp, WarpGroup.Ghost);
+                FilterLocalWarps(_other, warp, WarpGroup.AliveOther);
+            }
+        }
+
+        private void FilterLocalWarps(Dictionary<string, List<GhostWarp>> groups, GhostWarp warp, WarpGroup? group)
+        {
+            if (group != null && !warp.Group.HasFlag(group))
+               return;
+
+            if (!groups.TryGetValue(warp.SubGroup, out var list))
+            {
+                list = [];
+                groups.Add(warp.SubGroup, list);
+            }
+
+            list.Add(warp);
+        }
+
+        // WWDP EDIT END
+
+        public void UpdateWarps(List<GhostWarp> warps)
+        {
+            // WWDP EDIT START
+            _originalGhostWarps = warps;
+            Populate();
+            // WWDP EDIT END
         }
 
         private void AddButtons()
         {
-            foreach (var (name, warpTarget) in _warps)
-            {
-                var currentButtonRef = new Button
-                {
-                    Text = name,
-                    TextAlign = Label.AlignMode.Right,
-                    HorizontalAlignment = HAlignment.Center,
-                    VerticalAlignment = VAlignment.Center,
-                    SizeFlagsStretchRatio = 1,
-                    MinSize = new Vector2(340, 20),
-                    ClipText = true,
-                };
-
-                currentButtonRef.OnPressed += _ => WarpClicked?.Invoke(warpTarget);
-                currentButtonRef.Visible = ButtonIsVisible(currentButtonRef);
-
-                ButtonContainer.AddChild(currentButtonRef);
-            }
-        }
-
-        private bool ButtonIsVisible(Button button)
-        {
-            return string.IsNullOrEmpty(_searchText) || button.Text == null || button.Text.Contains(_searchText, StringComparison.OrdinalIgnoreCase);
-        }
-
-        private void UpdateVisibleButtons()
-        {
-            foreach (var child in ButtonContainer.Children)
-            {
-                if (child is Button button)
-                    button.Visible = ButtonIsVisible(button);
-            }
+            // WWDP EDIT START
+            AddButtons(_aliveAntags, "ghost-teleport-menu-antagonists-label");
+            AddButtons(_alivePlayers, "ghost-teleport-menu-alive-label"); // Alive
+            AddButtons(_ghostPlayers, "ghost-teleport-menu-ghosts-label"); // Ghost
+            AddButtons(_leftPlayers, "ghost-teleport-menu-left-label", true); // Left
+            AddButtons(_deadPlayers, "ghost-teleport-menu-dead-label", true); // Dead
+            AddButtons(_places, "ghost-teleport-menu-locations-label", true);
+            AddButtons(_other, "ghost-teleport-menu-other-label", true);
+            // WWDP EDIT END
         }
 
         private void OnSearchTextChanged(LineEdit.LineEditEventArgs args)
         {
-            _searchText = args.Text;
-
-            UpdateVisibleButtons();
+            // WWDP EDIT START
+            _searchText = args.Text.ToLower();
+            Populate();
+            // WWDP EDIT END
         }
+
+        // WWDP EDIT START
+
+        private void AddButtons(Dictionary<string, List<GhostWarp>> sortedWarps, string text, bool enableOtherCategory = false)
+        {
+            if(sortedWarps.Count == 0)
+                return;
+
+            var mainContainer = new BoxContainer()
+            {
+                Orientation = BoxContainer.LayoutOrientation.Vertical,
+                HorizontalExpand = true,
+                SeparationOverride = 5
+            };
+
+            var header = CreateSectionHeader(text);
+            mainContainer.AddChild(header);
+
+            var otherContainer = new FlexBox()
+            {
+                AlignContent = FlexBox.FlexAlignContent.SpaceBetween
+            };
+
+            foreach (var (subCategory, warps) in sortedWarps)
+            {
+                if(warps.Count == 0)
+                    continue;
+
+                var isOtherContainer = enableOtherCategory && (string.IsNullOrEmpty(subCategory) || warps.Count == 1);
+
+                var subBox = new FlexBox()
+                {
+                    AlignContent = FlexBox.FlexAlignContent.SpaceBetween
+                };
+
+                var departmentLabel = new Label
+                {
+                    Text = Loc.GetString(subCategory) + ": " + warps.Count,
+                    StyleClasses = { "LabelSecondaryColor" }
+                };
+
+                foreach (var warp in warps)
+                {
+                    var playerButton = new Button
+                    {
+                        Text = warp.DisplayName,
+                        TextAlign = Label.AlignMode.Right,
+                        HorizontalAlignment = HAlignment.Center,
+                        VerticalAlignment = VAlignment.Center,
+                        SizeFlagsStretchRatio = 1,
+                        ModulateSelfOverride = warp.Color,
+                        ToolTip = Loc.GetString(warp.Description),
+                        TooltipDelay = 0.1f,
+                        SetWidth = 180,
+                        ClipText = true,
+                    };
+
+                    playerButton.Label.ModulateSelfOverride = GetTextColor(warp.Color);
+                    playerButton.OnPressed += _ => WarpClicked?.Invoke(warp.Entity);
+
+                    if(!isOtherContainer)
+                        subBox.AddChild(playerButton);
+                    else
+                        otherContainer.AddChild(playerButton);
+                }
+
+                if(!isOtherContainer)
+                {
+                    mainContainer.AddChild(departmentLabel);
+                    mainContainer.AddChild(subBox);
+                }
+            }
+
+            if (enableOtherCategory && otherContainer.ChildCount > 0)
+            {
+                var otherLabel = new Label
+                {
+                    Text = Loc.GetString("ghost-teleport-menu-other-label") + ": " + otherContainer.ChildCount,
+                    StyleClasses = { "LabelSecondaryColor" }
+                };
+
+                mainContainer.AddChild(otherLabel);
+                mainContainer.AddChild(otherContainer);
+            }
+
+            GhostTeleportContainer.AddChild(mainContainer);
+        }
+
+        private Control CreateSectionHeader(string text, bool useStripeBack = true)
+        {
+            var label = new Label
+            {
+                Text = Loc.GetString(text),
+                StyleClasses = { "LabelBig" },
+                Align = Label.AlignMode.Center,
+            };
+
+            if (!useStripeBack)
+                return label;
+
+            var stripe = new StripeBack()
+            {
+                HorizontalExpand = true
+            };
+            stripe.AddChild(label);
+
+            return stripe;
+        }
+
+        public static Color GetTextColor(Color? background)
+        {
+            if (background is null)
+                return Color.White;
+
+            var luminance = (0.299 * background.Value.R +
+                0.587 * background.Value.G +
+                0.114 * background.Value.B);
+
+            return luminance > 0.5 ? Color.Black : Color.White;
+        }
+        //WWDP EDIT END
     }
 }

Content.Shared/_White/Roles/RoleAddingEvent.cs
@@ -1,0 +1,8 @@
+using Content.Shared.Mind;
+using Content.Shared.Roles;
+
+
+namespace Content.Shared._White.Roles;
+
+
+public sealed record RoleAddingEvent(EntityUid MindId, MindComponent MindComponent, EntityUid RoleUid, MindRoleComponent RoleComponent) : RoleEvent(MindId, MindComponent, false);

Content.Shared/_White/Roles/RoleCacheComponent.cs
@@ -1,0 +1,21 @@
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+
+namespace Content.Shared._White.Roles;
+
+[RegisterComponent]
+public sealed partial class RoleCacheComponent : Component
+{
+    [ViewVariables(VVAccess.ReadOnly), Access(typeof(RolesCacheSystem))]
+    public int AntagWeight;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public bool IsAntag => AntagWeight > 0;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public ProtoId<AntagPrototype>? LastAntagPrototype { get; [Access(typeof(RolesCacheSystem))] set; }
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public ProtoId<JobPrototype>? LastJobPrototype { get; [Access(typeof(RolesCacheSystem))] set; }
+}

Content.Shared/_White/Roles/RoleRemovingEvent.cs
@@ -1,0 +1,8 @@
+using Content.Shared.Mind;
+using Content.Shared.Roles;
+
+
+namespace Content.Shared._White.Roles;
+
+
+public sealed record RoleRemovingEvent(EntityUid MindId, MindComponent MindComponent, EntityUid RoleUid, MindRoleComponent RoleComponent) : RoleEvent(MindId, MindComponent, false);

Content.Shared/_White/Roles/RolesCacheSystem.cs
@@ -1,0 +1,40 @@
+using Content.Shared.Mind;
+using Content.Shared.Roles;
+
+
+namespace Content.Shared._White.Roles;
+
+
+public sealed class RolesCacheSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MindComponent, RoleAddingEvent>(OnRoleAdded);
+        SubscribeLocalEvent<MindComponent, RoleRemovingEvent>(OnRoleRemoved);
+    }
+
+    private void OnRoleRemoved(EntityUid uid, MindComponent mind, RoleRemovingEvent args)
+    {
+        if (!TryComp<RoleCacheComponent>(mind.CurrentEntity, out var component))
+            return;
+
+        if (args.RoleComponent.Antag)
+            component.AntagWeight -= 1;
+    }
+
+    private void OnRoleAdded(EntityUid uid, MindComponent component, RoleAddingEvent args)
+    {
+        if (component.CurrentEntity is null)
+            return;
+
+        var cacheComp = EnsureComp<RoleCacheComponent>(component.CurrentEntity.Value);
+
+        if (args.RoleComponent.Antag)
+            cacheComp.AntagWeight += 1;
+
+        if (args.RoleComponent.JobPrototype != null)
+            cacheComp.LastJobPrototype = args.RoleComponent.JobPrototype;
+        if (args.RoleComponent.AntagPrototype != null)
+            cacheComp.LastAntagPrototype = args.RoleComponent.AntagPrototype;
+    }
+}

AtaraxiaSpaceFoundation/Orion-Station-14

Co-Authored-By: PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
Co-Authored-By: Spatison <137375981+Spatison@users.noreply.github.com>

Content.Client/UserInterface/Systems/Ghost/Controls/GhostTargetWindow.xaml.cs
@@ -17,10 +17,11 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using System.Linq;
-using System.Numerics;
+using Content.Client._Orion.UserInterface.Controls;
+using Content.Client.UserInterface.Controls;
 using Content.Shared.Ghost;
 using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Client.UserInterface.XAML;
@@ -30,8 +31,20 @@ namespace Content.Client.UserInterface.Systems.Ghost.Controls
     [GenerateTypedNameReferences]
     public sealed partial class GhostTargetWindow : DefaultWindow
     {
-        private List<(string, NetEntity)> _warps = new();
         private string _searchText = string.Empty;
+
+        // Orion-Start
+        private List<GhostWarp> _originalGhostWarps = [];
+
+        private Dictionary<string, List<GhostWarp>> _alivePlayers = [];
+        private Dictionary<string, List<GhostWarp>> _deadPlayers = [];
+        private Dictionary<string, List<GhostWarp>> _ghostPlayers = [];
+        private Dictionary<string, List<GhostWarp>> _leftPlayers = [];
+
+        private Dictionary<string, List<GhostWarp>> _aliveAntags = [];
+        private Dictionary<string, List<GhostWarp>> _places = [];
+        private Dictionary<string, List<GhostWarp>> _other = [];
+        // Orion-End
 
         public event Action<NetEntity>? WarpClicked;
         public event Action? OnGhostnadoClicked;
@@ -44,73 +57,214 @@ namespace Content.Client.UserInterface.Systems.Ghost.Controls
             GhostnadoButton.OnPressed += _ => OnGhostnadoClicked?.Invoke();
         }
 
-        public void UpdateWarps(IEnumerable<GhostWarp> warps)
-        {
-            // Server COULD send these sorted but how about we just use the client to do it instead
-            _warps = warps
-                .OrderBy(w => w.IsWarpPoint)
-                .ThenBy(w => w.DisplayName, Comparer<string>.Create(
-                    (x, y) => string.Compare(x, y, StringComparison.Ordinal)))
-                .Select(w =>
-                {
-                    var name = w.IsWarpPoint
-                        ? Loc.GetString("ghost-target-window-current-button", ("name", w.DisplayName))
-                        : w.DisplayName;
-
-                    return (name, w.Entity);
-                })
-                .ToList();
-        }
-
         public void Populate()
         {
-            ButtonContainer.DisposeAllChildren();
+            // Orion-Edit-Start
+            GhostTeleportContainer.DisposeAllChildren();
+            FilterWarps();
             AddButtons();
+            // Orion-Edit-End
+        }
+
+        // Orion-Start
+        private void FilterWarps()
+        {
+
+            _alivePlayers.Clear();
+            _deadPlayers.Clear();
+            _ghostPlayers.Clear();
+            _leftPlayers.Clear();
+            _aliveAntags.Clear();
+            _places.Clear();
+            _other.Clear();
+
+            foreach (var warp in _originalGhostWarps)
+            {
+                if(!string.IsNullOrEmpty(_searchText) && !warp.DisplayName.Contains(_searchText, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                if(warp.Group == WarpGroup.Location)
+                {
+                    FilterLocalWarps(_places, warp, null);
+                    continue;
+                }
+
+                FilterLocalWarps(_alivePlayers, warp, WarpGroup.AliveDepartment);
+                FilterLocalWarps(_deadPlayers, warp, WarpGroup.DeadDepartment);
+                FilterLocalWarps(_leftPlayers, warp, WarpGroup.LeftDepartment);
+
+                FilterLocalWarps(_aliveAntags, warp, WarpGroup.AliveAntag);
+                FilterLocalWarps(_deadPlayers, warp, WarpGroup.DeadAntag);
+
+                FilterLocalWarps(_ghostPlayers, warp, WarpGroup.Ghost);
+                FilterLocalWarps(_other, warp, WarpGroup.AliveOther);
+            }
+        }
+
+        private void FilterLocalWarps(Dictionary<string, List<GhostWarp>> groups, GhostWarp warp, WarpGroup? group)
+        {
+            if (group != null && !warp.Group.HasFlag(group))
+               return;
+
+            if (!groups.TryGetValue(warp.SubGroup, out var list))
+            {
+                list = [];
+                groups.Add(warp.SubGroup, list);
+            }
+
+            list.Add(warp);
+        }
+        // Orion-End
+
+        public void UpdateWarps(List<GhostWarp> warps)
+        {
+            // Orion-Edit-Start
+            _originalGhostWarps = warps;
+            Populate();
+            // Orion-Edit-End
         }
 
         private void AddButtons()
         {
-            foreach (var (name, warpTarget) in _warps)
-            {
-                var currentButtonRef = new Button
-                {
-                    Text = name,
-                    TextAlign = Label.AlignMode.Right,
-                    HorizontalAlignment = HAlignment.Center,
-                    VerticalAlignment = VAlignment.Center,
-                    SizeFlagsStretchRatio = 1,
-                    MinSize = new Vector2(340, 20),
-                    ClipText = true,
-                };
-
-                currentButtonRef.OnPressed += _ => WarpClicked?.Invoke(warpTarget);
-                currentButtonRef.Visible = ButtonIsVisible(currentButtonRef);
-
-                ButtonContainer.AddChild(currentButtonRef);
-            }
-        }
-
-        private bool ButtonIsVisible(Button button)
-        {
-            return string.IsNullOrEmpty(_searchText) || button.Text == null || button.Text.Contains(_searchText, StringComparison.OrdinalIgnoreCase);
-        }
-
-        private void UpdateVisibleButtons()
-        {
-            foreach (var child in ButtonContainer.Children)
-            {
-                if (child is Button button)
-                    button.Visible = ButtonIsVisible(button);
-            }
+            // Orion-Edit-Start
+            AddButtons(_aliveAntags, "ghost-teleport-menu-antagonists-label");
+            AddButtons(_alivePlayers, "ghost-teleport-menu-alive-label"); // Alive
+            AddButtons(_ghostPlayers, "ghost-teleport-menu-ghosts-label"); // Ghost
+            AddButtons(_leftPlayers, "ghost-teleport-menu-left-label", true); // Left
+            AddButtons(_deadPlayers, "ghost-teleport-menu-dead-label", true); // Dead
+            AddButtons(_places, "ghost-teleport-menu-locations-label", true);
+            AddButtons(_other, "ghost-teleport-menu-other-label", true);
+            // Orion-Edit-End
         }
 
         private void OnSearchTextChanged(LineEdit.LineEditEventArgs args)
         {
-            _searchText = args.Text;
-
-            UpdateVisibleButtons();
-            // Reset scroll bar so they can see the relevant results.
-            GhostScroll.SetScrollValue(Vector2.Zero);
+            // Orion-Edit-Start
+            _searchText = args.Text.ToLower();
+            Populate();
+            // Orion-Edit-End
         }
+
+        // Orion-Edit-Start
+        private void AddButtons(Dictionary<string, List<GhostWarp>> sortedWarps, string text, bool enableOtherCategory = false)
+        {
+            if(sortedWarps.Count == 0)
+                return;
+
+            var mainContainer = new BoxContainer()
+            {
+                Orientation = BoxContainer.LayoutOrientation.Vertical,
+                HorizontalExpand = true,
+                SeparationOverride = 5
+            };
+
+            var header = CreateSectionHeader(text);
+            mainContainer.AddChild(header);
+
+            var otherContainer = new FlexBox()
+            {
+                AlignContent = FlexBox.FlexAlignContent.SpaceBetween
+            };
+
+            foreach (var (subCategory, warps) in sortedWarps)
+            {
+                if(warps.Count == 0)
+                    continue;
+
+                var isOtherContainer = enableOtherCategory && (string.IsNullOrEmpty(subCategory) || warps.Count == 1);
+
+                var subBox = new FlexBox()
+                {
+                    AlignContent = FlexBox.FlexAlignContent.SpaceBetween
+                };
+
+                var departmentLabel = new Label
+                {
+                    Text = Loc.GetString(subCategory) + ": " + warps.Count,
+                    StyleClasses = { "LabelSecondaryColor" }
+                };
+
+                foreach (var warp in warps)
+                {
+                    var playerButton = new Button
+                    {
+                        Text = warp.DisplayName,
+                        TextAlign = Label.AlignMode.Right,
+                        HorizontalAlignment = HAlignment.Center,
+                        VerticalAlignment = VAlignment.Center,
+                        SizeFlagsStretchRatio = 1,
+                        ModulateSelfOverride = warp.Color,
+                        ToolTip = Loc.GetString(warp.Description),
+                        TooltipDelay = 0.1f,
+                        SetWidth = 180,
+                        ClipText = true,
+                    };
+
+                    playerButton.Label.ModulateSelfOverride = GetTextColor(warp.Color);
+                    playerButton.OnPressed += _ => WarpClicked?.Invoke(warp.Entity);
+
+                    if(!isOtherContainer)
+                        subBox.AddChild(playerButton);
+                    else
+                        otherContainer.AddChild(playerButton);
+                }
+
+                if(!isOtherContainer)
+                {
+                    mainContainer.AddChild(departmentLabel);
+                    mainContainer.AddChild(subBox);
+                }
+            }
+
+            if (enableOtherCategory && otherContainer.ChildCount > 0)
+            {
+                var otherLabel = new Label
+                {
+                    Text = Loc.GetString("ghost-teleport-menu-other-label") + ": " + otherContainer.ChildCount,
+                    StyleClasses = { "LabelSecondaryColor" }
+                };
+
+                mainContainer.AddChild(otherLabel);
+                mainContainer.AddChild(otherContainer);
+            }
+
+            GhostTeleportContainer.AddChild(mainContainer);
+        }
+        // Orion-Edit-End
+
+        // Orion-Start
+        private Control CreateSectionHeader(string text, bool useStripeBack = true)
+        {
+            var label = new Label
+            {
+                Text = Loc.GetString(text),
+                StyleClasses = { "LabelBig" },
+                Align = Label.AlignMode.Center,
+            };
+
+            if (!useStripeBack)
+                return label;
+
+            var stripe = new StripeBack()
+            {
+                HorizontalExpand = true
+            };
+            stripe.AddChild(label);
+
+            return stripe;
+        }
+
+        public static Color GetTextColor(Color? background)
+        {
+            if (background is null)
+                return Color.White;
+
+            var luminance = (0.299 * background.Value.R +
+                0.587 * background.Value.G +
+                0.114 * background.Value.B);
+
+            return luminance > 0.5 ? Color.Black : Color.White;
+        }
+        // Orion-End
     }
 }

Content.Shared/_Orion/Roles/RoleAddingEvent.cs
@@ -1,0 +1,6 @@
+using Content.Shared.Mind;
+using Content.Shared.Roles;
+
+namespace Content.Shared._Orion.Roles;
+
+public sealed record RoleAddingEvent(EntityUid MindId, MindComponent MindComponent, EntityUid RoleUid, MindRoleComponent RoleComponent) : RoleEvent(MindId, MindComponent, false);

Content.Shared/_Orion/Roles/RoleCacheComponent.cs
@@ -1,0 +1,20 @@
+using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Orion.Roles;
+
+[RegisterComponent]
+public sealed partial class RoleCacheComponent : Component
+{
+    [ViewVariables(VVAccess.ReadOnly), Access(typeof(RolesCacheSystem))]
+    public int AntagWeight;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public bool IsAntag => AntagWeight > 0;
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public ProtoId<AntagPrototype>? LastAntagPrototype { get; [Access(typeof(RolesCacheSystem))] set; }
+
+    [ViewVariables(VVAccess.ReadOnly)]
+    public ProtoId<JobPrototype>? LastJobPrototype { get; [Access(typeof(RolesCacheSystem))] set; }
+}

Content.Shared/_Orion/Roles/RoleRemovingEvent.cs
@@ -1,0 +1,6 @@
+using Content.Shared.Mind;
+using Content.Shared.Roles;
+
+namespace Content.Shared._Orion.Roles;
+
+public sealed record RoleRemovingEvent(EntityUid MindId, MindComponent MindComponent, EntityUid RoleUid, MindRoleComponent RoleComponent) : RoleEvent(MindId, MindComponent, false);

Content.Shared/_Orion/Roles/RolesCacheSystem.cs
@@ -1,0 +1,37 @@
+using Content.Shared.Mind;
+
+namespace Content.Shared._Orion.Roles;
+
+public sealed class RolesCacheSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MindComponent, RoleAddingEvent>(OnRoleAdded);
+        SubscribeLocalEvent<MindComponent, RoleRemovingEvent>(OnRoleRemoved);
+    }
+
+    private void OnRoleRemoved(EntityUid uid, MindComponent mind, RoleRemovingEvent args)
+    {
+        if (!TryComp<RoleCacheComponent>(mind.CurrentEntity, out var component))
+            return;
+
+        if (args.RoleComponent.Antag)
+            component.AntagWeight -= 1;
+    }
+
+    private void OnRoleAdded(EntityUid uid, MindComponent component, RoleAddingEvent args)
+    {
+        if (component.CurrentEntity is null)
+            return;
+
+        var cacheComp = EnsureComp<RoleCacheComponent>(component.CurrentEntity.Value);
+
+        if (args.RoleComponent.Antag)
+            cacheComp.AntagWeight += 1;
+
+        if (args.RoleComponent.JobPrototype != null)
+            cacheComp.LastJobPrototype = args.RoleComponent.JobPrototype;
+        if (args.RoleComponent.AntagPrototype != null)
+            cacheComp.LastAntagPrototype = args.RoleComponent.AntagPrototype;
+    }
+}