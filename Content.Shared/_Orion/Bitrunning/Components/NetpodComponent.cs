40f9be13108f1c548f601eba5a45f444daad5b13
PuroSlavKing <puroslavking@yahoo.com>
2026-05-02 20:00:41 +0300

    up33

--- Content.Shared/_Orion/Bitrunning/Components/NetpodComponent.cs
+++ Content.Shared/_Orion/Bitrunning/Components/NetpodComponent.cs
@@ -1,4 +1,3 @@
-using Content.Shared.Implants;
 using Content.Shared.Roles;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
@@ -19,7 +18,7 @@ public sealed partial class NetpodComponent : Component
     public EntityUid? Avatar;
 
     [DataField, AutoNetworkedField]
-    public ProtoId<StartingGearPrototype>? PreferredLoadout = "ShaftMinerGear";
+    public ProtoId<StartingGearPrototype>? PreferredLoadout = "BitrunnerAvatarShaftMinerGear"; // TODO: Replace with BitrunnerGear
 
     [DataField]
     public List<ProtoId<StartingGearPrototype>> AllowedLoadout = new();
---
+++ Resources/Locale/ru-RU/ss14-ru/prototypes/_orion/entities/markers/spawners/bitrunning.ftl
@@ -0,0 +1,8 @@
+ent-BitrunningExitMarker = маркер выхода
+ent-BitrunningGoalMarker = маркер цели
+ent-BitrunningObjectiveEncryptedCacheSpawnMarker = спавнер зашифрованного кэша
+ent-BitrunningObjectiveCacheCrateSpawnMarker = спавнер ящика кэша
+ent-BitrunningRewardCacheSpawnMarker = спавнер наградного ящика кэша
+ent-BitrunningAvatarSpawnMarker = маркер спавна аватара
+ent-BitrunningObjectiveCacheDeliveryPointMarker = маркер доставки кэша
+ent-BitrunningEncryptedCacheObjectiveSpawner = спавнер зашифрованного кэша
