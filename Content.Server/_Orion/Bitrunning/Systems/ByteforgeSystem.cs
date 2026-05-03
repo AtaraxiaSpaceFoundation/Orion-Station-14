fae4ce7388f7127cfe5c45e8ef093b163f6367a6
PuroSlavKing <puroslavking@yahoo.com>
2026-05-03 23:46:58 +0300

    up1

--- Content.Server/_Orion/Bitrunning/Systems/ByteforgeSystem.cs
+++ Content.Server/_Orion/Bitrunning/Systems/ByteforgeSystem.cs
@@ -69,14 +69,12 @@ public sealed class ByteforgeSystem : EntitySystem
         {
             oldByteforgeComp.LinkedServer = null;
             _appearance.SetData(oldByteforge, ByteforgeVisuals.ByteforgeAngry, false);
-            Dirty(oldByteforge, oldByteforgeComp);
         }
 
         ent.Comp.LinkedByteforge = args.Sink;
         byteforge.LinkedServer = ent.Owner;
         UpdateByteforgeEmagVisual(ent.Comp);
         Dirty(ent);
-        Dirty(args.Sink, byteforge);
     }
 
     private void OnServerPortDisconnected(Entity<QuantumServerComponent> ent, ref PortDisconnectedEvent args)
@@ -85,10 +83,7 @@ public sealed class ByteforgeSystem : EntitySystem
             return;
 
         if (TryComp<ByteforgeComponent>(args.RemovedPortUid, out var byteforge))
-        {
             byteforge.LinkedServer = null;
-            Dirty(args.RemovedPortUid, byteforge);
-        }
 
         if (ent.Comp.LinkedByteforge is { } oldLinked && Exists(oldLinked))
             _appearance.SetData(oldLinked, ByteforgeVisuals.ByteforgeAngry, false);
@@ -196,7 +191,6 @@ public sealed class ByteforgeSystem : EntitySystem
                 ent.Comp.LinkedByteforge = linkedEntity;
                 byteforge.LinkedServer = ent.Owner;
                 UpdateByteforgeEmagVisual(ent.Comp);
-                Dirty(linkedEntity, byteforge);
                 Dirty(ent);
                 return;
             }
