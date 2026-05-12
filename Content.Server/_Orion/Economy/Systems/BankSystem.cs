fd00afd001e20a63fe28683189c224d59de8d491
PuroSlavKing <puroslavking@yahoo.com>
2026-05-12 22:39:43 +0300

    up6.3

--- Content.Server/_Orion/Economy/Systems/BankSystem.cs
+++ Content.Server/_Orion/Economy/Systems/BankSystem.cs
@@ -31,21 +31,11 @@ public sealed class BankSystem : EntitySystem
     {
         var account = EnsureComp<StationAccountComponent>(mindUid);
 
-        var dirty = false;
         if (!IsValidAccountId(account.AccountId))
-        {
             account.AccountId = GenerateUniqueAccountId();
-            dirty = true;
-        }
 
         if (Resolve(mindUid, ref mind, false) && !string.IsNullOrWhiteSpace(mind.CharacterName) && account.OwnerName != mind.CharacterName)
-        {
             account.OwnerName = mind.CharacterName;
-            dirty = true;
-        }
-
-        if (dirty)
-            Dirty(mindUid, account);
 
         return account;
     }
