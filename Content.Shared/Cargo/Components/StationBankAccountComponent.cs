f8ef289bdc72c439cbd81dd29d86d2e87c84aeb5
PuroSlavKing <puroslavking@yahoo.com>
2026-05-13 23:06:00 +0300

    up8.2.1

--- Content.Shared/Cargo/Components/StationBankAccountComponent.cs
+++ Content.Shared/Cargo/Components/StationBankAccountComponent.cs
@@ -81,7 +81,6 @@ public sealed partial class StationBankAccountComponent : Component
     /// <summary>
     /// Next payout time for prototype-driven periodic budget funding.
     /// </summary>
-    [DataField, AutoNetworkedField]
     public Dictionary<ProtoId<CargoAccountPrototype>, TimeSpan> NextBudgetFundingTime = new();
     // Orion-End
 }
