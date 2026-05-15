44253488dfe1d9e197f5680733ca46bdcccab254
PuroSlavKing <puroslavking@yahoo.com>
2026-05-15 22:21:55 +0300

    up14.2.1

--- Content.Shared/Cargo/CargoOrderData.cs
+++ Content.Shared/Cargo/CargoOrderData.cs
@@ -151,8 +151,11 @@ namespace Content.Shared.Cargo
         // Orion-Start
         public void SetPrivateBuyerData(string accountId, string buyerName)
         {
-            ArgumentNullException.ThrowIfNull(accountId);
-            ArgumentNullException.ThrowIfNull(buyerName);
+            if (accountId == null)
+                throw new ArgumentNullException(nameof(accountId));
+
+            if (buyerName == null)
+                throw new ArgumentNullException(nameof(buyerName));
 
             if (string.IsNullOrWhiteSpace(accountId))
                 throw new ArgumentException("Private buyer account ID cannot be empty.", nameof(accountId));
