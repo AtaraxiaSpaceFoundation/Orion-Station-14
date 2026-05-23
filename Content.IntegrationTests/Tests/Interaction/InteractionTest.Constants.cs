8996e8c4963342b3872abe4bd080373984cfad10
PuroSlavKing <puroslavking@yahoo.com>
2026-05-23 20:27:02 +0300

    up18.5

--- Content.IntegrationTests/Tests/Interaction/InteractionTest.Constants.cs
+++ Content.IntegrationTests/Tests/Interaction/InteractionTest.Constants.cs
@@ -46,6 +46,9 @@ public abstract partial class InteractionTest
     protected const string MatterBin2 = "AdvancedMatterBinStockPart";
     protected const string MatterBin3 = "SuperMatterBinStockPart";
     protected const string MatterBin4 = "BluespaceMatterBinStockPart";
+    protected const string Capacitor1 = "CapacitorStockPart";
+    protected const string ScanningModule1 = "ScanningModuleStockPart";
+    protected const string MicroLaser1 = "MicroLaserStockPart";
     // Orion-End
     protected const string Battery1 = "PowerCellSmall";
     protected const string Battery4 = "PowerCellHyper";
