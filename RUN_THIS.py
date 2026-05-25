0121836d7345e4e69e057db88f3c5bb7ef89ab64
M9SCO <48180788+M9SCO@users.noreply.github.com>
2026-05-25 14:52:44 +0600

    Update RUN_THIS.py

--- RUN_THIS.py
+++ RUN_THIS.py
@@ -19,4 +19,6 @@ if version.major < 3 or (version.major == 3 and version.minor < 5):
     print("ERROR: You need at least Python 3.5 to build SS14.")
     sys.exit(1)
 
+# -- run
+
 subprocess.run([sys.executable, "git_helper.py"], cwd="BuildChecker")
